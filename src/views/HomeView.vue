4705066adf705112ea03b959f2d67f5a648637d3
Karl <karljoyeux@gmail.com>
2025-04-07 19:18:24 +0200

    views are correctly setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import TheHomePage from '@/components/pages/TheHomePage.vue';
 </script>
 
 <template>
   <main>
+    <the-home-page/>
   </main>
 </template>
--- src/views/SkillView.vue
+++ src/views/SkillView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <the-skills-page/>
+  </main>
 </template>
-
+<script lang="ts" setup>
+import TheSkillsPage from '@/components/pages/TheSkillsPage.vue';
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
